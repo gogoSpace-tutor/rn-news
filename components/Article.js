7b912255ef02291745bcce039d62bd707d6df450
gogoSpace-tutor <gogoSpace01@gmail.com>
2023-05-30 22:48:23 +0300

    text container

--- components/Article.js
+++ components/Article.js
@@ -12,8 +12,19 @@ const Article = () => {
           style={styles.image}
         />
       </View>
-      <Text style={styles.title}>News Application</Text>
-      <Text style={styles.description}>Here we'll have a discription </Text>
+      <View style={styles.textContainer}>
+        <Text style={styles.title}>News Application</Text>
+        <Text style={styles.description}>Here we'll have a discription </Text>
+        
+        <View style={styles.data}>
+            <Text style={styles.heading}>by: <Text style={styles.value}>Author</Text></Text>
+            <Text style={styles.date}>June 01 2023</Text>
+        </View>
+        
+        <View style={styles.sourceContainer} >
+          <Text>source: <Text style={styles.source}>some source</Text></Text>
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
@@ -27,8 +38,8 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   },
   imageContainer: {
-    width: '90%',
-    aspectRatio: 16 / 9, // Пропорції зображення
+    width: '95%',
+    aspectRatio: 16 / 9, 
     alignSelf: 'center',
   },
   image: {
@@ -36,6 +47,9 @@ const styles = StyleSheet.create({
     width: undefined,
     height: undefined,
     resizeMode: 'cover',
+  },
+  textContainer: {
+    padding: 10,
   },
   title: {
     fontSize: 18,
@@ -46,5 +60,31 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: '400',
     marginTop: 10,
+  },
+  data: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 10,
+  },
+  heading: {
+
+  },
+  author: {
+    fontWeight: '700',
+
+  },
+  date: {
+    fontWeight: '700',
+    color: 'red',
+    fontSize: 16,
+  },
+  source: {
+    marginTop: 10,
+    color: 'red',
+    fontSize: 16,
+  },
+  sourceContainer: {
+    marginTop: 10,
   }
+
 });
